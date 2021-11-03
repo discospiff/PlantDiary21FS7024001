18781248da492ba2ede482a27ef8dcad0f3e03e3
jonesbr <brandan.jones@uc.edu>
2021-11-02 20:44:27 -0400

    Use a dictionary to filter the specimens by the water loving plants.

--- PlantDiary21FS7024001/Pages/Index.cshtml.cs
+++ PlantDiary21FS7024001/Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using plantfeed;
 using specimenfeed;
 
 namespace PlantDiary21FS7024001.Pages
@@ -30,6 +31,22 @@ namespace PlantDiary21FS7024001.Pages
 
             using (var webClient = new WebClient())
             {
+                // raw string data of plants that like water.
+                string plantsJSON = webClient.DownloadString("http://plantplaces.com/perl/mobile/viewplantsjsonarray.pl?WetTolerant=on");
+
+                // put the thirsty plants into a collection.
+                List<Plant> plants = Plant.FromJson(plantsJSON);
+
+                // init our dictionary
+                IDictionary<long, Plant> allPlants = new Dictionary<long, Plant>();
+
+                // load the plants into the dictionary.
+                foreach(Plant plant in plants)
+                {
+                    allPlants.Add(plant.Id, plant);
+                }
+
+
                 // grab our JSON text. 
                 var specimenJSON = webClient.DownloadString("https://www.plantplaces.com/perl/mobile/viewspecimenlocations.pl?Lat=39.14455075&Lng=-84.5093939666667&Range=0.5&Source=location&Version=2");
 
@@ -39,7 +56,17 @@ namespace PlantDiary21FS7024001.Pages
                 // let's get our specimens
                 List<Specimen> specimens = specimenCollection.Specimens;
 
-                ViewData["Specimens"] = specimens;
+                // filter the specimens to those that like water.
+                List<Specimen> waterMeSpecimens = new List<Specimen>();
+                foreach(Specimen specimen in specimens)
+                {
+                    if(allPlants.ContainsKey(specimen.PlantId))
+                    {
+                        waterMeSpecimens.Add(specimen);
+                    }
+                }
+
+                ViewData["Specimens"] = waterMeSpecimens;
             }
 
         }
